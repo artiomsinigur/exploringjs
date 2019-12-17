8c71947e9144af6571a04631c0f94437c442ebe4
Artiom <sinigur2006@gmail.com>
2019-12-16 21:57:13 -0500

    The non-values undefined and null

--- summary.js
+++ summary.js
@@ -367,3 +367,17 @@
           true
           Object.is(123, '123')
           false
+
+
+  // ========================
+  // 13 The non-values undefined and null
+  // ========================
+
+    // ## 13.1 undefined vs. null
+    // =========================
+      // The language itself makes the following distinction:
+      // * undefined means “not initialized” (e.g., a variable) or “not existing” (e.g., a property of an object).
+      // * null      means “the intentional absence of any object value” (a quote from the language specification).
+        let foo;                // undefined
+        function bar() {}       // undefined
+        const baz = (x) => x;   // undefined
