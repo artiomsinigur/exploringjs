b08b7e97c8bb19334e2638bfce3c0ff37e06dca7
Artiom <sinigur2006@gmail.com>
2019-12-17 23:55:20 -0500

    Exercice_1 Check if isObject with switch statement

--- exercises/control-flow/is_object_via_switch.mjs
+++ exercises/control-flow/is_object_via_switch.mjs
@@ -1,3 +1,10 @@
 export function isObject(x) {
-  return x !== null && (typeof x === 'object' || typeof x === 'function');
+  // return x !== null && (typeof x === 'object' || typeof x === 'function');
+  switch (typeof x) {
+    case 'object':
+    case 'function':
+      return x !== null;
+    default:
+      return false;
+  }
 }
